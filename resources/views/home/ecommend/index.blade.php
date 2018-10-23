b2a10695117c6a5b56058c2cbd75290c7d4dd3e7
lihuiqiang2001 <904785685@qq.com>
2018-10-23 20:41:40 +0800

    强1

--- resources/views/home/ecommend/index.blade.php
+++ resources/views/home/ecommend/index.blade.php
@@ -1,44 +1,26 @@
 @extends('layout.home')
 
-@section('title', $title)
+@section('title',$title)
 
 @section('content')
-<div class="mws-panel grid_8">
-	<div class="mws-panel-header">
-    	<span>{{$title}}</span>
-    </div>
-    <div class="mws-panel-body no-padding">
-
-		@if (count($errors) > 0)
-		    <div class="mws-form-message error">
-	        	错误信息
-	            <ul>
-	            	@foreach ($errors->all() as $error)
-		                <li>{{ $error }}</li>
-		            @endforeach
-	            </ul>
-	        </div>
-		@endif
-	</div>
-</div>
-
-<div class="top-nav bg3">
-	<div class="nav-box inner">
-		<div class="all-cat">
-			<div class="title"><i class="iconfont icon-menu"></i> 全部分类</div>
+	<!-- 内页导航栏 -->
+	<div class="top-nav bg3">
+		<div class="nav-box inner">
+			<div class="all-cat">
+				<div class="title"><i class="iconfont icon-menu"></i> 全部分类</div>
+			</div>
+			<ul class="nva-list">
+				<a href="index.html"><li>首页</li></a>
+				<a href="temp_article/udai_article10.html"><li>企业简介</li></a>
+				<a href="temp_article/udai_article5.html"><li>新手上路</li></a>
+				<a href="class_room.html"><li>U袋学堂</li></a>
+				<a href="enterprise_id.html"><li>企业账号</li></a>
+				<a href="udai_contract.html"><li>诚信合约</li></a>
+				<a href="item_remove.html"><li>实时下架</li></a>
+			</ul>
 		</div>
-		<ul class="nva-list">
-			<a href="index.html"><li>首页</li></a>
-			<a href="temp_article/udai_article10.html"><li>企业简介</li></a>
-			<a href="temp_article/udai_article5.html"><li>新手上路</li></a>
-			<a href="class_room.html"><li>U袋学堂</li></a>
-			<a href="enterprise_id.html"><li>企业账号</li></a>
-			<a href="udai_contract.html"><li>诚信合约</li></a>
-			<a href="item_remove.html"><li>实时下架</li></a>
-		</ul>
 	</div>
-</div>
-<div class="content inner">
+	<div class="content inner">
 		<section class="item-show__div item-show__head clearfix">
 			<div class="pull-left">
 				<ol class="breadcrumb">
@@ -55,14 +37,14 @@
 						<a href="javascript:;" class="btn btn-default btn-prev"></a>
 						<div class="thumb-list">
 							<ul class="wrapper clearfix">
-							@foreach($spicture as $k=>$v)
+								@foreach($spicture as $k=>$v)
 								<li class="item active" data-src="/{{$v->gpic}}"><img class="cover" src="/{{$v->gpic}}" alt="商品预览图"></li>
-							@endforeach
+								@endforeach
 							</ul>
 						</div>
 						<a href="javascript:;" class="btn btn-default btn-next"></a>
 					</div>
-					<div class="big-box"><img src="" alt="重回汉唐 旧忆 原创设计日常汉服女款绣花长褙子吊带改良宋裤春夏"></div>
+					<div class="big-box"><img src="/{{$ecommend->picture}}" alt="{{$ecommend->gname}}"></div>
 				</div>
 				<script src="/home/js/jquery.magnifier.js"></script>
 				<script>
@@ -78,7 +60,7 @@
 					<div class="item-price bgf5">
 						<div class="price-box clearfix">
 							<div class="price-panel pull-left">
-								售价：<span class="price">￥{{$ecommend->price}} </span>
+								售价：<span class="price">￥{{$ecommend->price}}<s class="fz16 c9">￥{{($ecommend->price)*1.5}}</s></span>
 							</div>
 							<div class="vip-price-panel pull-right">
 								会员等级价格 <i class="iconfont icon-down"></i>
@@ -110,7 +92,7 @@
 					</div>
 					<ul class="item-ind-panel clearfix">
 						<li class="item-ind-item">
-							<span class="ind-label c9">累计销量</span>
+							<span class="ind-label c9">库存</span>
 							<span class="ind-count cr">{{$ecommend->stock}}</span>
 						</li>
 						<li class="item-ind-item">
@@ -123,10 +105,25 @@
 						</li>
 					</ul>
 					<div class="item-key">
-						<div class="item-sku">
-							
-						
-						</div>
+						<!-- <div class="item-sku">
+							<dl class="item-prop clearfix">
+								<dt class="item-metatit">颜色：</dt>
+								<dd><ul data-property="颜色" class="clearfix">
+									<li><a class="on" href="javascript:;" role="button" data-value="白色" aria-disabled="true">
+										<span>白色</span>
+									</a></li>
+									<li><a href="javascript:;" role="button" data-value="黑色" aria-disabled="true">
+										<span>黑色</span>
+									</a></li>
+									<li><a href="javascript:;" role="button" data-value="粉红色" aria-disabled="true">
+										<span>粉红色</span>
+									</a></li>
+									<li><a href="javascript:;" role="button" data-value="黄色" aria-disabled="true">
+										<span>黄色</span>
+									</a></li>
+								</ul></dd>
+							</dl>
+						</div> -->
 						<div class="item-amount clearfix bgf5">
 							<div class="item-metatit">数量：</div>
 							<div class="amount-box">
@@ -183,17 +180,16 @@
 					<span class="c6">爆款推荐</span>
 				</div>
 				<div class="swiper-container picked-swiper">
-
 					<div class="swiper-wrapper">
 						<div class="swiper-slide">
-						@foreach($stock as $k=>$v)
-							<a class="picked-item" href="/home/ecommend/{{$v->id}}">
+							@foreach($stock as $k=>$v)
+							<a class="picked-item" href="/home/goods/details/{{$v->id}}">
 								<img src="/{{$v->picture}}" alt="" class="cover">
-								<div class="look_price">{{$v->price}}</div>
+								<div class="look_price">¥{{$v->price}}</div>
 							</a>
-						@endforeach	
+							@endforeach
+
 						</div>
-						
 					</div>
 				</div>
 				<div class="picked-button-prev"></div>
@@ -216,7 +212,7 @@
 				<ul class="nav-tabs nav-pills clearfix" role="tablist" id="item-tabs">
 					<li role="presentation" class="active"><a href="#detail" role="tab" data-toggle="tab" aria-controls="detail" aria-expanded="true">商品详情</a></li>
 					<li role="presentation"><a href="#evaluate" role="tab" data-toggle="tab" aria-controls="evaluate">累计评价 <span class="badge">1314</span></a></li>
-					
+					<li role="presentation"><a href="#service" role="tab" data-toggle="tab" aria-controls="service">售后服务</a></li>
 				</ul>
 			</div>
 			<div class="pull-left">
@@ -225,22 +221,24 @@
 						<div class="item-detail__info clearfix">
 							<div class="record">商品编号：D-88{{$ecommend->id}}</div>
 							<div class="record">上架时间：{{$ecommend->addtime}}</div>
-							<div class="record">商品毛重：200克</div>
-							<div class="record">商品库存：{{$ecommend->stock}}</div>
+							<div class="record">商品毛重：2000克</div>
+							<div class="record">商品库存：{{$ecommend->stock}}件</div>
 						</div>
 						<div class="rich-text">
 							<p style="text-align: center;">
 								<i id="desc-module-1" style="font-size: 0"></i>
-
 								{!!$ecommend->content!!}
-
 								<i id="desc-module-2" style="font-size: 0"></i>
+								
 								
 								<i id="desc-module-3" style="font-size: 0"></i>
 								
+								
 								<i id="desc-module-4" style="font-size: 0"></i>
 								
+								
 								<i id="desc-module-5" style="font-size: 0"></i>
+								
 							</p>
 						</div>
 					</div>
@@ -1081,7 +1079,9 @@
 					</div>
 					<div role="tabpanel" class="tab-pane fade" id="service" aria-labelledby="service-tab">
 						<!-- 富文本 -->
-						
+						<div class="service-content rich-text">
+							<img title="" alt="" src="http://img.aocmonitor.com.cn/image/2014-06/86575417.gif" width="240" height="160" border="0" align="left"><p>承蒙惠购 AOC 产品，谨致谢意！为了让您更好地使用本产品，武汉艾德蒙科技股份有限公司通过该产品随机附带的保修证向您做出下述维修服务承诺，并按照该服务的承诺向您提供维修服务。</p><p>这些服务承诺仅适用于2016年6月1日（含）之后销售的AOC品牌显示器标准品。</p><p>如果您选择购买了 AOC 显示器扩展功能模块或其它厂家电脑主机，其保修承诺请参见相应产品的保修卡。</p><p>所有承诺内容以产品附件的保修卡为准。</p><p><br></p><h3>一、全国联保</h3><p style="text-indent:2em">AOC 显示器实施全国范围联保，国家标准三包服务。无论您是在中国大陆 ( 不含香港、澳门、台湾地区) 何处购买并在大陆地区使用的显示器，出现三包范围内的故障时，可凭显示器的保修证正本和购机发票到 AOC 显示器维修网点或授权网点进行维修同时，也欢迎您关注官方微信服务号“AOC用户俱乐部”(微信号：aocdisplay)进行查询。</p><div style="text-align:center"><img src="http://img.aocmonitor.com.cn/image/2017-05/89154415.jpg" alt=""></div><p><br></p><p>三包服务如下：</p><ol><li>商品自售出之日起 7 日内，出现《微型计算机商品性能故障表》中所列故障时，消费者可选择退货、换货或修理。</li><li>商品自售出之日起 15 日内，出现《微型计算机商品性能故障表》中所列故障时，消费者可选择换货或修理。</li><li>商品自售出之日起 1 年内，出现《微型计算机商品性能故障表》中所列故障时，消费者可选择修理。</li></ol><p>以下情况不在三包范围内：</p><ol><li>超过三包有效期。</li><li>无有效的三包凭证及发票。</li><li>发票上内容与商品实物标识不符或者涂改的。</li><li>未按产品使用说明书要求使用、维护、保养而造成损坏的（人为损坏）。</li><li>非 AOC 授权的修理者拆动造成损坏的（私自拆修）。</li><li>非 AOC 在中国大陆（不含香港、澳门、台湾地区）销售的商品。</li></ol><h3>二、显示器专享服务</h3><p><strong>1、LUVIA视界头等舱，VIP专享服务</strong></p><p style="text-indent:2em">AOC针对各省市地区采取指定商品销售，消费者购买指定销往该区域的LUVIA卢瓦尔显示器标准品，从发票开具之日起1年内，注册成为官方微信服务号“AOC用户俱乐部”(微信号：aocdisplay)产品会员，即可在当地享“LUVIA视界头等舱，VIP专享服务”。</p><div style="text-align:center"><img src="http://img.aocmonitor.com.cn/image/2017-05/25352146.jpg" alt=""></div><p><br></p><p style="text-indent:2em">* 如客户未在发票开具之日起1年内注册AOC微信会员，则只享受国家三包服务。</p><p style="text-indent:2em">注册会员方式：1、关注“AOC用户俱乐部”微信公众号。2、点击“会员”→“注册会员”。3、填写个人真实信息并注册产品信息，即可成为AOC产品会员。</p><p style="text-indent:2em"><strong>3年免费上门更换</strong>：从发票开具之日起3年内，产品若发生《微型计算机商品性能故障表》所列性能故障，可免费更换不低于同型号同规格产品。（服务网点无法覆盖区域，全国区域免费邮寄，双向运费由AOC负担）</p><p style="text-indent:2em"><strong>一键快捷掌上服务：</strong>从注册成为“AOC用户俱乐部”会员之日起，可享在线贴身技术顾问有问必答、售后服务在线预约、服务网点在线查询等一键快捷掌上服务。（人工客服咨询在线时间：8:00-22:00）</p><p style="text-indent:2em"><strong>增值豪礼尊享服务：</strong>可参加“AOC用户俱乐部”有奖互动赢取豪礼。</p><p>注：<br>(1)如不能及时提供购机发票或发票记载不清、涂改、商品实物标示和发票内容不符，将以您上传“AOC用户俱乐部”的发票信息为准计算保修时间；如果发票信息并未上传，将以该显示器制造日期(制造日期见显示器后壳条形码标签)加一个月为准计算保修时间。<br>(2)非“AOC用户俱乐部”产品会员，不享受“LUVIA视界头等舱，VIP专享服务”。</p>
+						</div>
 					</div>
 			    </div>
 				<div class="recommends">
@@ -1091,12 +1091,12 @@
 					<div class="swiper-container recommends-swiper">
 						<div class="swiper-wrapper">
 							<div class="swiper-slide">
-							@foreach($stock as $k=>$v)
-								<a class="picked-item" href="/home/ecommend/{{$v->id}}">
+								@foreach($stock as $k=>$v)
+								<a class="picked-item" href="/home/goods/details/{{$v->id}}">
 									<img src="/{{$v->picture}}" alt="" class="cover">
 									<div class="look_price">¥{{$v->price}}</div>
 								</a>
-							@endforeach	
+								@endforeach
 							</div>
 						</div>
 					</div>
@@ -1121,10 +1121,10 @@
 								<a href="#desc-module-2"><i class="iconfont icon-selected"></i> 细节图</a>
 							</dd>
 							<dd class="dc-idsItem">
-								<a href="#desc-module-3"><i class="iconfont"></i> 尺寸及试穿</a>
+								<a href="#desc-module-3"><i class="iconfont"></i> 尺寸</a>
 							</dd>
 							<dd class="dc-idsItem">
-								<a href="#desc-module-4"><i class="iconfont"></i> 模特效果图</a>
+								<a href="#desc-module-4"><i class="iconfont"></i> 效果图</a>
 							</dd>
 							<dd class="dc-idsItem">
 								<a href="#desc-module-5"><i class="iconfont"></i> 常见问题</a>
@@ -1248,5 +1248,12 @@
 			$(document).ready(function(){ $('.to-top').toTop({position:false}) });
 		</script>
 	</div>
+
+	
+	
+
+
+
+
 
 @stop
